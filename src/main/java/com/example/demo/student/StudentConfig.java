4e58c1850a35f8d81260b1b155ea4d47e92f7fb4
joao.hass <joaopedrohass@outlook.com>
2023-05-20 01:25:55 -0300

    feat: Added StudentConfig to generate generate mock Students
    It uses a commandLineRunner class as a @Bean to generate them and save to the StudentRepository

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student john = new Student(
+                    "John",
+                    "john@gmail.com",
+                    LocalDate.of(2002, Month.MAY, 5),
+                    21);
+
+            Student doe = new Student(
+                    "doe",
+                    "doe@outlook.com",
+                    LocalDate.of(2001, Month.JANUARY, 10),
+                    22
+            );
+
+            repository.saveAll(List.of(john, doe));
+        };
+    }
+}
