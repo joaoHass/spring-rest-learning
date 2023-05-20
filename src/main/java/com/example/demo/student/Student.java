282fb59a3c52ff8c930e140ea192dcedc90db725
joao.hass <joaopedrohass@outlook.com>
2023-05-19 23:37:34 -0300

    db: Adicionado anotações para geração de sequence na Student

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,16 +1,27 @@
 package com.example.demo.student;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 
 @Entity
 @Table
 public class Student {
+    @Id
+    @SequenceGenerator (
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue (
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
