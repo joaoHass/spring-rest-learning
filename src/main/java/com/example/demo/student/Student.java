548bee9a9795dbcc19956ada16559ce4d49219cb
joao.hass <joaopedrohass@outlook.com>
2023-05-20 00:03:55 -0300

    misc: Removed unused import on Student
    Was made unnecessary after commenting the @Table annotation (see commit 2e2af9e)

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 
