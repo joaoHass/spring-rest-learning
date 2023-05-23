967b3eeee7d3ded0426bdfcc4adaa43d067557db
joao.hass <joaopedrohass@outlook.com>
2023-05-22 22:23:36 -0300

    feat: Added method to update Student entity

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,13 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -38,5 +41,18 @@ public class StudentService {
             throw new IllegalArgumentException(String.format("A user with the id %s does not exist!", studentId));
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public void updateStudent(Integer studentId, StudentUpdateDTO studentDTO) throws IllegalArgumentException {
+
+        // TODO: Find a better way to write this method (including the DTO class)
+        Optional<Student> studentToUpdate = studentRepository.findById(studentId);
+
+        if (studentToUpdate.isEmpty())
+            throw new IllegalArgumentException(String.format("A user with the id %s does not exist!", studentId));
+
+        studentToUpdate.get().setName(studentDTO.getName());
+        studentToUpdate.get().setEmail(studentDTO.getEmail());
     }
 }
