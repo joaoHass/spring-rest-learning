94ef150201374d6292908bd2ce298a5dd2a9a912
joao.hass <joaopedrohass@outlook.com>
2023-05-22 22:22:57 -0300

    feat: Added StudentUpdateDTO class

---
+++ src/main/java/com/example/demo/student/StudentUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+public class StudentUpdateDTO {
+
+    private String name;
+    private String email;
+
+    public StudentUpdateDTO(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
